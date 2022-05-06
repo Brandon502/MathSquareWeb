15618e55f5ad65567810a696929a1cd54738d962
Brandon502 <105077712+Brandon502@users.noreply.github.com>
2022-05-06 17:25:59 -0400

    Update mathsquare.js

--- mathsquare.js
+++ mathsquare.js
@@ -1,67 +1,32 @@
+//TODO NEXT Add ng-module inputted numbers. Erase on new game.
+var app = angular.module('myApp', []);
+
+app.controller('mainCtrl', function($scope) {
+    $scope.newGame = function() {
+        game = createGame();
+        $scope.operators = game[1];
+        $scope.expectedValues = game[2];
+        $scope.displayOp = game[1].join('').replaceAll('*','×'); //Change * to ×
+    };
+    setUpInputNums();
+    setMainListener();
+    setInputListener();
+    $scope.newGame();
+});
+
+
 var activeNum = null;
 
-var GAME = {
-    inputed: ["", "", "",
-              "", "", "",
-              "", "", ""],
-    operators: ["+", "+",
-                "-", "-", "-",
-                "+", "+",
-                "-", "-", "-",
-                "+", "+"],
-    expectedValues: [10, 10, 10, 10, 10, 10, 10]
+function setMainListener() {
+    for (i = 0; i < 9; i++) {
+        $(`#mainNum${i}`).click(mainNumberClick);
+    };
 }
-
-
-window.onload = (event) => {
-    setUpMainGrid();
-    setUpInputNums();
-};
-
-function setUpMainGrid() {
-    grid = document.getElementById('numberGrid');
-    
-    //GRID 7x7 49 divs
-    for (d = 0; d < 49; d++) {
-        div = document.createElement("div");
-        div.id = d;
-        div.classList.add("grid-item")
-        grid.append(div);
-    };      
-    //Main Numbers
-    mainNumbers = [0,2,4,14,16,18,28,30,32] //Number IDs
-    for (num in mainNumbers) {
-        div = document.getElementById(mainNumbers[num]);
-        div.id = `mainNum${num}`;
-        div.classList.add("mainNumber");
-        div.addEventListener("click", mainNumberClick);
-    };
-    //Operators
-    operators = [1,3,7,9,11,15,17,21,23,25,29,31] //Operator IDs
-    for (operator in operators){
-        div = document.getElementById(operators[operator]);
-        div.id = `op${operators[operator]}`
-        div.classList.add("operator");
-        div.innerHTML = "+";        
-    };
-
-    //Equals
-    equals = [5,19,33,35,37,39] //Eqal IDs
-    for (equ in equals) {
-        div = document.getElementById(equals[equ]);
-        div.classList.add("equals");
-        div.innerHTML = '=';
-    };
-    
-    //Values
-    values = [6,20,34,42,44,46] //Value IDs
-    for (value in values) {
-        div = document.getElementById(values[value]);
-        div.id = `value${value}`
-        div.classList.add("values");
-        div.innerHTML = 10;
-    };
-};
+function setInputListener() {
+    for (i=1; i<10; i++) {
+        $(`#inputNum${i}`).click(inputNumberClick);
+    }
+}
 
 function setUpInputNums() {
     inputDiv = document.getElementById("inputNumbers")
@@ -70,10 +35,10 @@ function setUpInputNums() {
         div.id = `inputNum${i}`;
         div.classList.add("inputNumber");
         div.innerHTML = i;
-
         inputDiv.append(div);
     }
 };
+
 function mainNumberClick() {
     if (activeNum != null) {
         previous = document.getElementById(activeNum);
@@ -110,6 +75,9 @@ document.addEventListener('keydown', (event) =>{
     }
 })
 
+function inputNumberClick() {
+    inputNumber(this.innerHTML);
+};
 
 // Input Number Function Controls Grid Pencilmark Layout
 function inputNumber(num) {
@@ -119,18 +87,15 @@ function inputNumber(num) {
     
     numbers = numbers.replace(/&nbsp;/gi, '');
     numbers = numbers.replace(/<br>/gi,'');
-    console.log(numbers);
 
-    if (numbers.includes(num)) {
-        
+
+    if (numbers.includes(num)) {      
         numbers = numbers.replace(num,'')
-
     }
     else {
         numbers += num
     }
-    console.log (numbers.length);
-    
+   
     if (numbers.length == 1) {
         cell.innerHTML = numbers;
         cell.classList.add("regularInput");
@@ -165,3 +130,82 @@ function changeActiveCell(increment) {
     
     document.getElementById(num).dispatchEvent(new Event('click'));
 }
+
+function createGame(weights = [.25, .25, .25, .25, .25, .25, .25, .25]) { //weights currently ignored
+
+    checkLoop:
+    for(x=0; x<1000; x++) {
+        numbers = shuffle();
+        operators = [];
+        for (i = 0; i < 12; i++) {
+            rand = Math.random();
+            
+            if (rand < .25) {
+                operators.push('+');
+            }
+            else if (rand < .5) {
+                operators.push('-');
+            }
+            else if (rand < .75) {
+                operators.push('*');
+            }
+            else {
+                operators.push('/');
+            }
+
+        }
+
+        //Rows and Columns
+        rc = [
+            `(${numbers[0]} ${operators[0]} ${numbers[1]}) ${operators[1]} ${numbers[2]}`,   //Row 1
+            `(${numbers[3]} ${operators[5]} ${numbers[4]}) ${operators[6]} ${numbers[5]}`,   //Row 2
+            `(${numbers[6]} ${operators[10]} ${numbers[7]}) ${operators[11]} ${numbers[8]}`, //Row 3
+            `(${numbers[0]} ${operators[2]} ${numbers[3]}) ${operators[7]} ${numbers[6]}`,   //Col 1
+            `(${numbers[1]} ${operators[3]} ${numbers[4]}) ${operators[8]} ${numbers[7]}`,   //Col 2
+            `(${numbers[2]} ${operators[4]} ${numbers[5]}) ${operators[9]} ${numbers[8]}`    //Col 3             
+        ]
+
+        for(check in rc) {
+            if (countDecimals(eval(rc[check])) > 4) { //DECIMAL CHECK
+                continue checkLoop //Too many decimals restart loop
+            }
+        }
+        
+        //All Values Passed Decimal Check
+        values = []
+        for(value in rc) {
+            values.push(eval(rc[value]))
+        }
+
+        return [numbers, operators, values];
+    }
+
+}
+
+function shuffle() {
+    let numbers = [1,2,3,4,5,6,7,8,9];
+    let shuffled = numbers
+        .map(value => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value);
+    return shuffled;
+}
+
+function countDecimals(value) {
+    if (Math.floor(value) !== value)
+        return value.toString().split(".")[1].length || 0;
+    return 0;
+}    
+// function evaluateMath(num, op, location) {
+//     // Takes in a number list, operators list and location such as 'r1' and evaulates the equation
+//     Locations = {
+//         'r1': `${num[0]} ${op[0]} ${num[1]} ${op[1]} ${num[2]}`,
+//         'r2': `${num[3]} ${op[5]} ${num[4]} ${op[6]} ${num[5]}`,
+//         'r3': `${num[6]} ${op[10]} ${num[7]} ${op[11]} ${num[8]}`,
+//         'c1': `${num[0]} ${op[2]} ${num[3]} ${op[7]} ${num[6]}`,
+//         'c2': `${num[1]} ${op[3]} ${num[4]} ${op[8]} ${num[7]}`,
+//         'c3': `${num[2]} ${op[4]} ${num[5]} ${op[9]} ${num[8]}`     
+//     }
+
+//     return eval(Locations[location])
+// }
